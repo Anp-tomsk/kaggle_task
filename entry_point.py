408cbc9a45b9d477570651fb3955db4e99f64741
Anp-mcc <andrey.popov@mcc-tomsk.de>
2017-03-06 23:12:26 +0700

    wrap medical appointment to py-object

--- entry_point.py
+++ entry_point.py
@@ -1,4 +1,33 @@
+from models import Constants
+from models.AppointmentInfo import build_info
+from itertools import islice
 
+
+def input_to_dict(line):
+    args = line.split(',')
+    return {
+        Constants.AGE: args[0],
+        Constants.GENDER: args[1],
+        Constants.DATE_OF_REGISTRATION: args[2],
+        Constants.APPOINTMENT_DATE: args[3],
+        Constants.DAY_OF_WEEK: args[4],
+        Constants.DISEASES : {
+            Constants.DIABETES: args[6],
+            Constants.ALCOHOLISM: args[7],
+            Constants.HYPER_TENSION: args[8],
+            Constants.HANDICAP: args[9],
+            Constants.SMOKES: args[10],
+            Constants.SCHOLARSHIP: args[11],
+            Constants.TUBERCULOSIS: args[12]
+        },
+        Constants.SMS_REMINDER: args[13],
+        Constants.TIME_OF_WAITING: args[14]
+    }
 
 if __name__ == "__main__":
-    pass
+    with open('data/medical_appointment.csv', 'r') as iFile:
+            lines = list(islice(iFile, 2))[1:]
+            for line in lines:
+                args = input_to_dict(line)
+                info = build_info(**args)
+                print(info.age)
--- models/AppointmentInfo.py
+++ models/AppointmentInfo.py
@@ -1,20 +1,47 @@
 from enum import Enum
+from models import Constants
 import time
 
-mapping_info = {"age": lambda value: int(value),
-                "gender": lambda value: gender_from_str(value),
-                "date_of_registration": lambda value: time.strptime(value, "%Y-%m-%d"),
-                "appointment_date": lambda value: time.strptime(value, "%Y-%m-%d"),
-                "day_of_week": lambda value: value,
-                "show_up": lambda value: show_up_from_str(value),
-                "sms_reminder": lambda value: bool(value),
-                "time_of_waiting": lambda value: int(value),
-                "diseases": lambda **kwargs: get_diseases(kwargs)}
+
+mapping_info = {Constants.AGE: lambda value: int(value),
+                Constants.GENDER: lambda value: gender_from_str(value),
+                Constants.DATE_OF_REGISTRATION: lambda value: date_from_str(value),
+                Constants.APPOINTMENT_DATE: lambda value: date_from_str(value),
+                Constants.DAY_OF_WEEK: lambda value: value,
+                Constants.SHOW_UP: lambda value: show_up_from_str(value),
+                Constants.SMS_REMINDER: lambda value: bool(value),
+                Constants.TIME_OF_WAITING: lambda value: int(value),
+                Constants.DISEASES: lambda kwargs: get_diseases(kwargs)}
 
 
-def get_diseases(**kwargs):
-    for key, value in kwargs:
-        pass
+def date_from_str(value):
+    return time.strptime(value[0:10], "%Y-%m-%d")
+
+
+def get_diseases(kwargs):
+    diseases = []
+    for key, value in kwargs.items():
+        if key.lower() == Constants.DIABETES:
+            if bool(value):
+                diseases.append(Diseases.DIABETES)
+        if key.lower() == Constants.ALCOHOLISM:
+            if bool(value):
+                diseases.append(Diseases.ALCOHOLISM)
+        if key.lower() == Constants.HYPER_TENSION:
+            if bool(value):
+                diseases.append(Diseases.HYPER_TENSION)
+        if key.lower() == Constants.HANDICAP:
+            if bool(value):
+                diseases.append(Diseases.HANDICAP)
+        if key.lower() == Constants.SMOKES:
+            if bool(value):
+                diseases.append(Diseases.SMOKES)
+        if key.lower() == Constants.SCHOLARSHIP:
+            if bool(value):
+                diseases.append(Diseases.SCHOLARSHIP)
+        if key.lower() == Constants.TUBERCULOSIS:
+            if bool(value):
+                diseases.append(Diseases.TUBERCULOSIS)
 
 
 def gender_from_str(value):
@@ -47,9 +74,13 @@ class Diseases(Enum):
     TUBERCULOSIS = 7
 
 
+def build_info(**kwargs):
+    return AppointmentInfo(**kwargs)
+
+
 class AppointmentInfo:
     def __init__(self, **kwargs):
-        for key, value in kwargs:
+        for key in kwargs:
             if key in mapping_info:
                 value = kwargs[key]
                 setattr(self, key, mapping_info[key](value))
